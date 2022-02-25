c8add213779aae3a06ca58499bd9bd4ba2c2b680
David Michael II <knowdavidmichael@gmail.com>
2022-02-25 23:07:27 +0100

    feat: update banki_app with get route management

--- lib/app.dart
+++ lib/app.dart
@@ -1,16 +1,17 @@
 import 'package:banki_flutter/features/shop/presentation/pages/shop_page.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class BankiApp extends StatelessWidget {
   const BankiApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+    return GetMaterialApp(
+      title: 'Banki Online Store',
       home: const ShopPage(),
+      theme: ThemeData(primarySwatch: Colors.blue),
+      initialRoute: '/',
+      getPages: [GetPage(name: '/shop', page: () => const ShopPage())],
     );
   }
 }
