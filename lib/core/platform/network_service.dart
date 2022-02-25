5b77f3b410933fedc120addeffc64c98e3e2e1b6
David Michael II <knowdavidmichael@gmail.com>
2022-02-25 22:34:22 +0100

    feat: add injectable annotation to network service

--- lib/core/platform/network_service.dart
+++ lib/core/platform/network_service.dart
@@ -1,9 +1,11 @@
 import 'package:data_connection_checker_tv/data_connection_checker.dart';
+import 'package:injectable/injectable.dart';
 
 abstract class NetworkService {
   Future<bool>? get isConn;
 }
 
+@LazySingleton(as: NetworkService)
 class NetworkServiceImpl implements NetworkService {
   NetworkServiceImpl({required this.checker});
   final DataConnectionChecker checker;
