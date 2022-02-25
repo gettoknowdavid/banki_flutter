a9cfc85836b1063fa60998912e3933ecd807e4b7
David Michael II <knowdavidmichael@gmail.com>
2022-02-25 22:25:19 +0100

    feat: injectable for shop usecase

--- lib/features/shop/domain/usecases/get_all_products.dart
+++ lib/features/shop/domain/usecases/get_all_products.dart
@@ -3,7 +3,9 @@ import 'package:banki_flutter/core/usecase/usecase.dart';
 import 'package:banki_flutter/features/shop/domain/entities/product_response.dart';
 import 'package:banki_flutter/features/shop/domain/repositories/shop_repository.dart';
 import 'package:dartz/dartz.dart';
+import 'package:injectable/injectable.dart';
 
+@lazySingleton
 class GetAllProducts implements UseCase<ProductResponse?, NoParams> {
   GetAllProducts(this.repository);
 
